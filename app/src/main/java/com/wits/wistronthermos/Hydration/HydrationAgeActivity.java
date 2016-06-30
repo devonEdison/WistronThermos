4fdf92fd686b37b637c5bf6be4bf5a5ed7e755d0
Devon <cococodeco@gmail.com>
2016-06-30 20:37:30 +0800

    Hydration age update fix bug

--- app/src/main/java/com/wits/wistronthermos/Hydration/HydrationAgeActivity.java
+++ app/src/main/java/com/wits/wistronthermos/Hydration/HydrationAgeActivity.java
@@ -1,6 +1,8 @@
 package com.wits.wistronthermos.Hydration;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -13,26 +15,30 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class HydrationAgeActivity extends AppCompatActivity {
+	private static final String TAG = "HydrationAgeActivity";
 	TextView ageTextView,cancel, next;
+	DatePicker datePicker;
+	int yearCurrent,monthCurrent,dayCurrent,age=0;
+	SharedPreferences sharedPref;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_hydration_age);
-
-		DatePicker datePicker = (DatePicker) findViewById(R.id.datePicker);
-		datePicker.setMaxDate(new Date().getTime());
+		//set time
 		Calendar calendar = Calendar.getInstance();
-		final int yearCurrent = calendar.get(Calendar.YEAR);
-		int monthCurrent = calendar.get(Calendar.MONTH);
-		int dayCurrent = calendar.get(Calendar.DATE);
-
+		yearCurrent = calendar.get(Calendar.YEAR);
+		monthCurrent = calendar.get(Calendar.MONTH);
+		dayCurrent = calendar.get(Calendar.DATE);
+		//set age Text View
 		ageTextView = (TextView) findViewById(R.id.age_text);
-		ageTextView.setText("0");
+
+		datePicker = (DatePicker) findViewById(R.id.datePicker);
+		datePicker.setMaxDate(new Date().getTime());
 		datePicker.init(yearCurrent, monthCurrent, dayCurrent, new DatePicker.OnDateChangedListener() {
 			@Override
 			public void onDateChanged(DatePicker view, int yearChange, int monthOfYear, int dayOfMonth){
-				int age = yearCurrent - yearChange;
-				ageTextView.setText("" + age);
+				age = yearCurrent - yearChange;
+				ageTextView.setText(""+age);
 			}
 		});
 
@@ -48,11 +54,27 @@ public class HydrationAgeActivity extends AppCompatActivity {
 		next.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
+				//setup
+				SharedPreferences.Editor editor = sharedPref.edit();
+				editor.putString(getString(R.string.preference_user_age), String.valueOf(age));
+				editor.commit();
+
 				Intent intent = new Intent(HydrationAgeActivity.this, HydrationSexActivity.class);
 				startActivity(intent);
 				overridePendingTransition(0, 0);
 			}
 		});
+
+		sharedPref = HydrationAgeActivity.this.getSharedPreferences(
+				getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		if (ageTextView != null) {
+			ageTextView.setText("" + age);
+		}
 	}
 
 	@Override
